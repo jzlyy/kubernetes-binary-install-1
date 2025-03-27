05af3ad31dcb3519253dbc84968a8f1e57990bf9
root <wwyxhqc1@foxmail.com>
2025-03-27 18:14:47 +0800

    Pre-configure

---
+++ scripts/cluster/master/00-preflight.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+######Pre-configure######
+
+#Modify the hostname
+sudo hostnamectl set-hostname master-node1
+
+#Configure DNS resolution for the host
+sudo cat /root/kubernetes-binary-install-1/configs/hosts | sudo tee -a /etc/hosts >/dev/null
+
+#Disable SELinux (not recommended in production environments)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
+#Disable firewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+#Disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+#Reboot the system
+bash

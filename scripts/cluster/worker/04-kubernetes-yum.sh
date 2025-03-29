624fb4bb80aab02f829926ed8ae1108018ec24e7
root <wwyxhqc1@foxmail.com>
2025-03-29 15:40:13 +0800

    Configure Kubernetes YUM Repository

--- scripts/cluster/worker/04-kubernetes-yum.sh
+++ scripts/cluster/worker/04-kubernetes-yum.sh
@@ -2,5 +2,5 @@
 
 ######Configure Kubernetes YUM Repository######
 
-sudo cp /root/kubernetes-binary-install-1/configs/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
+sudo cp /root/kubernetes-binary-install1/configs/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
 sudo dnf makecache
